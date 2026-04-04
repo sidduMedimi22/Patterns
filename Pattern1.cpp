d34c087c6898a51d281fa49167424204e01d7750
MVS Siddharth <siddharthmvs22@gmail.com>
2026-04-04 23:49:02 +0530

    Add files via upload

---
+++ Pattern1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int n;
+    cout<<"enter value of n "<<"  ";
+    cin>>n;
+    while (i<=n){
+        int j=1;
+        while (j<=n){
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
